b8d12f338726075f793a6861b843d6cf205c0599
Luís Henrique Martins <38386755+luishacm@users.noreply.github.com>
2021-11-29 12:36:45 -0300

    Add files via upload

---
+++ read_text.py
@@ -0,0 +1,151 @@
+import pytesseract
+from PIL import Image
+import time
+from nltk.tokenize import sent_tokenize
+import pandas as pd
+import os
+
+#Select the correct path for your Tesseract instalation
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe' 
+
+#Selecting True for this variable will only get the text from an image, clean it and save it in the database
+#Selecting False for this variable will also ask you the sentiments of the text for each of its paragraphs
+only_clean = False
+
+#If you started determining the sentiment of the texts and closed the program this will help.
+#If choose this variable as True it will restart where you left last
+#However, you should always finish determining the sentiment of all the paragraphs of the same Article and stop at the start of the next article
+#Or else, it won't save the unfinished analysis of that article.
+restart_where_stoped = True
+
+#This function will transform the image given to it in text
+#It will return the raw reading
+def read_text(path):
+##    img = cv2.imread(path)
+##    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+##    ret,thresh1 = cv2.threshold(gray, 0, 255,cv2.THRESH_OTSU|cv2.THRESH_BINARY_INV)
+##    im = Image.fromarray(thresh1)
+    im = Image.open(path)
+    text = pytesseract.image_to_string(im, lang = 'por', config="--psm 3 --oem 3")
+    return text
+
+#This function will receive the Text and then clean it accordingly
+#Will remove unnecessary spaces, join words that were separated and separate each sentence in a different item of a list
+#It will return a list with each sentence inside
+def clean_text(text):
+    new_lista = []
+    index_lista = []
+    final_text = []
+    for i in text:
+        new_lista.append(i)
+    for i, j in enumerate(new_lista):
+        if j == "-":
+            index_lista.append(i)
+    final_text = text
+    x = 0
+    for i in index_lista:
+        final_text = final_text[:i-x] + final_text[i-x+2:]
+        x += 2            
+    final_text = sent_tokenize(final_text)
+    final_lista = []
+    for i in final_text:
+        repeat = 0
+        lista = []
+        for o in i:
+            if o == '\n':
+                if repeat == 1:
+                    continue
+                lista.append(" ")
+                repeat = 1
+            else:
+                repeat = 0
+                lista.append(o)
+        lista = ''.join(map(str, lista))
+        final_lista.append(lista)
+    return final_lista
+
+
+def select_text(text):
+    #This function is a filter
+    #It will return only the texts that contain the Keywords we are looking for
+    #If not, it will force the program to go to the next article
+    keywords = ["ministro", "ministério", "ministerio", "ministra", "itamaraty", "mec"]
+    for i in [paragraph.lower() for paragraph in text]:
+        for o in keywords:
+            if o in i:
+                return text        
+    not_selected = 0
+    return not_selected
+
+
+def select_sentiment(text, f):
+    #This function will ask you the sentiment of the text and each of its paragraphs
+    #Later it will add all this information to an Excel spreadsheet
+    origem = []
+    texto_pd = []
+    sentimento = []
+    text_sent = []
+    nome_jornal = []
+    data = []
+    print(text)
+    full_text_sent = input("Qual o sentimento geral desse texto? \n")
+    if full_text_sent == "ignore":
+        return False
+    for i in text:
+        print(i)
+        sentiment = input("Qual o sentimento desse trecho? \n")
+        nome_jornal.append(f[-20:-13])
+        data.append(f[-13:-4])
+        origem.append(f[:-4])
+        texto_pd.append(i)
+        sentimento.append(sentiment)
+        text_sent.append(full_text_sent)
+        
+    df_articles = pd.DataFrame(columns=['Origem', 'Data', 'Nome do Arquivo', 'Texto', 'Sentimento', 'Sentimento Geral'])
+    add_lists = {'Origem': nome_jornal, 'Data': data, 'Nome do Arquivo': origem, 'Texto': texto_pd, 'Sentimento': sentimento, 'Sentimento Geral': text_sent}
+    articles_organized = df_articles.append(pd.DataFrame(add_lists))
+    read_df = pd.read_excel('Planilhas/articles sentiment.xlsx', engine='openpyxl')
+    articles_sent = read_df.append(pd.DataFrame(articles_organized))
+    articles_sent.to_excel('Planilhas/articles sentiment.xlsx', encoding='utf-8', index=False)
+    return True
+
+if __name__ == '__main__':
+
+    #Here we are calling all these functions
+    path = "Database Ed/"    
+    files = os.listdir('Database Ed')
+    where_stoped = pd.read_excel('Planilhas/articles sentiment.xlsx', engine='openpyxl')
+    try:
+        where_stoped = where_stoped['Nome do Arquivo'].iloc[-1]
+    except Exception:
+        print("Your spreadsheet is empty, let's continue")
+        restart_where_stoped = False
+        where_stoped = "0"
+    
+    for f in files:
+        if f[:-4] != where_stoped and restart_where_stoped is True:
+            continue
+        elif f[:-4] == where_stoped and restart_where_stoped is True:
+            restart_where_stoped = False
+            continue
+        else:
+            pass
+        
+        file_path = (os.path.join(path, f))
+        start = time.time()
+        text = read_text(file_path)
+        end = time.time()
+        print(end - start)
+        text = clean_text(text)
+        text = select_text(text)
+        if text == 0:
+            continue
+        else:
+            str_text = lista = ' '.join(map(str, text))
+            with open("txt banco/{}.txt".format(f[:-4]), "w+", encoding='utf-8') as output:
+                output.write(str_text)
+        if only_clean is False:
+            approved = select_sentiment(text, f)
+            if approved is False:
+                os.remove("txt banco/{}.txt".format(f[:-4]))   
+
---
+++ sentiment_analysis.py
@@ -0,0 +1,87 @@
+import time
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import pandas as pd
+import random
+import math
+
+signs_to_delet = [' ', ',', '.', ':', ';', '—', '(', ')', 'r']
+stop_words = stopwords.words("portuguese")
+for sign in signs_to_delet:
+    stop_words.append(sign)
+df_sentiments = pd.read_excel('Planilhas/articles sentiment.xlsx', engine='openpyxl')
+pre_text_pos = [word_tokenize(f.lower()) for f in df_sentiments[(df_sentiments['Sentimento']==1)]['Texto'].tolist()]
+pre_text_neg = [word_tokenize(f.lower())  for f in df_sentiments[(df_sentiments['Sentimento']==-1)]['Texto'].tolist()]
+
+# pre_text_neg = pre_text_neg[:len(pre_text_pos)]
+# print(len(pre_text_pos))
+# print(len(pre_text_neg))
+
+#This will create a list of nouns, adverbs, verbs, etc
+# https://github.com/fmaruki/Nltk-Tagger-Portuguese
+# lines = 'lines is some string of words'
+# tokenized = nltk.word_tokenize(lines)
+# nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if(pos[:2] == 'NN')]
+# print (nouns)
+    
+text_pos = []
+for word_lista in pre_text_pos:
+    words = []
+    for word in word_lista:
+        if word not in stop_words:
+            words.append(word)
+    text_pos.append(words)
+
+text_neg = []
+for word_lista in pre_text_neg:
+    words = []
+    for word in word_lista:
+        if word not in stop_words:
+            words.append(word)
+    text_neg.append(words)
+
+documents = ([(name, 'pos') for name in text_pos] +
+             [(name, 'neg') for name in text_neg])
+
+random.shuffle(documents)
+
+all_words = []
+for text in text_pos:
+    for word in text:
+        all_words.append(word)
+for text in text_neg:
+    for word in text:
+        all_words.append(word)
+
+all_words = nltk.FreqDist(all_words)
+word_features = list(all_words.keys())[:2000]
+
+def find_features(documents):
+    #If the word is in the list of words we considerer to be relevant to the analysis, then it will be add them to the classifier as True, else, False.
+    words = set(documents)
+    features = {}
+    for w in word_features:
+        features[w] = (w in words)
+    return features
+
+#Creating a features set with True or False. If it is contained in the features, then the word will be True, if not, then False. 
+#This will give enough info to the algorythm to create a classifier.
+featuresets = [(find_features(rev), category) for (rev, category) in documents] 
+
+#Separating a training set and a testing set. Since it is already shuffled, we are just dividing it in two.
+separation = math.floor(len(featuresets)/2)
+training_set = featuresets[:separation]
+testing_set = featuresets[separation:]
+
+#Naive Bayes: posterior = prior occurences * likelihood / evidence 
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+print("Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(classifier, testing_set)*100))
+classifier.show_most_informative_features(15)
+
+def form_sent(sent):
+    return {word: True for word in nltk.word_tokenize(sent)}
+
+text = form_sent("Quando estiver concluído, o Rodoanel terá 174 quilômetros de extensão, interligando dez rodovias. O governador Alckmin tem demonstrado que é possível construir e preservar o meio ambiente, como foi feito, por exemplo, na nova pista da Imigrantes. Processos técnicos muito modernos impediram que as águas drenadas durante as escavações contaminas sem os córregos da região.")
+a = classifier.classify(text)
+print(a)
